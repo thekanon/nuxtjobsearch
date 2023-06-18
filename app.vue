bf9047f7f3a301fb46e578c79aac9246007c6457
thekanon <dlengjs123@gmail.com>
2023-06-18 22:16:46 +0900

    쿼카 분리

--- app.vue
+++ app.vue
@@ -1,71 +1,14 @@
 <template>
-  <header>
-    app.vue
-    <nav>
-      <ul>
-        라우팅
-        <li><NuxtLink to="/a">그냥 라우팅</NuxtLink></li>
-        <li><NuxtLink to="/story">다이나믹 라우팅</NuxtLink></li>
-        <li><NuxtLink to="/post-2/doo">중첩 라우팅</NuxtLink></li>
-        <li><NuxtLink to="/post-aa/112312/312">slug 라우팅1</NuxtLink></li>
-        <li><NuxtLink to="/pc/post/너무 우울해요">slug 라우팅2</NuxtLink></li>
-        <li><NuxtLink to="/pc/post/doodoo/야근">slug 라우팅3</NuxtLink></li>
-        <li>
-          <NuxtLink to="/%EC%95%88%EB%9D%BC%EC%9A%B0%ED%8C%85"
-            >라우팅테스트</NuxtLink
-          >
-        </li>
-      </ul>
-      <ul>
-        상태관리 + 네비게이션 가드
-        <li>
-          <NuxtLink to="/login/인간"
-            >쿼카만 통과 가능한 로그인에 인간접속</NuxtLink
-          >
-        </li>
-        <li>
-          <NuxtLink to="/login/쿼카"
-            >쿼카만 통과 가능한 로그인에 쿼카접속</NuxtLink
-          >
-        </li>
-        <li>
-          <NuxtLink to="/login/로니">로니 접속</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/%EC%9E%85%EC%96%91%EC%B2%98">쿼카 입양처</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-
+  <!-- <header>app.vue</header>
+  <NextHead /> -->
   <div>
     <NuxtPage />
   </div>
 </template>
 <script setup>
 import Layout from "~/layouts/defaultLayout.vue";
-import { useRouter } from "vue-router";
-import login from "~/middleware/login";
-import { useCounterStore } from "@/stores/counter";
-const router = useRouter();
 
-const search = ref("쿼카로니니");
-
-addRouteMiddleware(login);
-router.beforeEach((to, from, next) => {
-  const counter = useCounterStore();
-  console.log(to, from);
-  if (to.params.loginUser) {
-    if (to.params.loginUser.indexOf("쿼카")) {
-      console.log(counter.count);
-      counter.clearCount();
-      alert("인간은 쿼카굴에 못들어갑니다. 쿼카가 모두 도망갔습니다.");
-      return abortNavigation("/");
-    } else {
-      alert("쿼카의 서식지입니다.");
-      // return navigateTo(to.path);
-    }
-  }
-  next();
-});
+const NextHead = defineAsyncComponent(() =>
+  import("~/layouts/defaultLayout.vue")
+);
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,47 @@
-<template>index.vue</template>
+<template>
+  <div class="container">
+    <h1 class="title">타이틀</h1>
+    <div class="search">
+      <input type="text" v-model="searchText" placeholder="검색하세요" />
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "IndexPage",
+  setup() {
+    const searchText = ref("");
+
+    const search = () => {
+      console.log(searchText.value); // 검색 로직 구현
+    };
+
+    return {
+      searchText,
+      search,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+  background-color: #333;
+  color: #f8f8f8;
+}
+
+.title {
+  margin-bottom: 1rem;
+}
+
+.search input {
+  margin-right: 1rem;
+}
+</style>
